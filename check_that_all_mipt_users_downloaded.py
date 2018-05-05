c8e2718fbca3555c909d39cf3279dc73addd6ba9
Dannil <danokhlopkov@gmail.com>
2018-05-05 21:06:34 +0300

    add script to check if all mipt users parsed

---
+++ check_that_all_mipt_users_downloaded.py
@@ -0,0 +1,11 @@
+from tqdm import tqdm
+
+from utils import *
+from instaget import *
+
+apis = get_apis()
+
+mipt_users_id = extract_mipt_users()
+for user_id in tqdm(mipt_users_id):
+    _ = get_user_info(next(apis), user_id=user_id)
+    sleep(2)
