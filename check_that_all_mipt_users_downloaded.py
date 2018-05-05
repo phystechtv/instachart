0e8f624e84d488ff9f0dbca670a4ad30fba4b917
Dannil <danokhlopkov@gmail.com>
2018-05-05 21:25:32 +0300

    speed improve

--- check_that_all_mipt_users_downloaded.py
+++ check_that_all_mipt_users_downloaded.py
@@ -6,6 +6,6 @@ from instaget import *
 apis = get_apis()
 
 mipt_users_id = extract_mipt_users()
-for user_id in tqdm(mipt_users_id):
-    _ = get_user_info(next(apis), user_id=user_id)
-    sleep(2)
+for user_id in tqdm(mipt_users_id[::-1]):
+    if get_user_info(next(apis), user_id=user_id):
+        sleep(1)
