22a56bbfcf0b4b55b332d1e95a88758f0e1dbf32
Dannil <danokhlopkov@gmail.com>
2018-05-05 21:06:53 +0300

    upd script for new functions

--- populate_with_mipt_users.py
+++ populate_with_mipt_users.py
@@ -1,24 +1,36 @@
-from utils import *
-from instaparse import *
 from random import sample
 from collections import Counter
+
+from utils import *
+from instaget import *
 
 SAMPLE_SIZE = 10
 
 apis = get_apis()
+init_db()
 
-mipt_users = get_parsed_mipt_users()
+mipt_users = extract_mipt_users_info()
+# if not mipt_users:
+#     # for the first launch
+#     proven_users = ["ohld", "caffeinum", "mipt_physchem", "mipt.ru", "mipt_bm", "mipt_drec", "cheer_delta",
+#                    "belka_fbmf", "alphadance_cheer_mipt", "faki_mipt", "lavash_mipt", "miptculture",
+#                    "mipt_profkom", "kvn_mipt", "spektr_mipt", "miptstream_ru", "__hazerblu", "fiztehradio"]
+#     _ = [get_user_info(next(apis), username=u) for u in proven_users]
+#     mipt_users = [{"user_id": get_user_id(next(apis), username=u)} for u in proven_users]
+#     _ = [make_user_mipt(u["user_id"]) for u in mipt_users]
+
 
 all_followings = []
-mipt_users_sample = sample(mipt_users, SAMPLE_SIZE)
-for user in mipt_users_sample:
-    followings = next(apis).get_total_followings(user["user_id"])
+for user in sample(mipt_users, SAMPLE_SIZE):
+    _ = get_user_info(next(apis), user_id=user["user_id"])
+    followings = get_user_followings(next(apis), user_id=user["user_id"])
     if followings:
-        all_followings.extend([f["pk"] for f in followings])
-    sleep(2)
+        all_followings.extend(followings)
+    sleep(5)
 
 mipt_users_ids = set([u["user_id"] for u in mipt_users])
 value_counts = Counter([uid for uid in all_followings if uid not in mipt_users_ids])
 for user_id, count in value_counts.items():
     if count >= SAMPLE_SIZE / 2:
-        add_mipt_user(next(apis), user_id=user_id)
+        _ = get_user_info(next(apis), user_id=user_id)
+        _ = make_user_mipt(user_id)
