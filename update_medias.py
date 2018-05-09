addbd26f06944399c0756a2ff081eb0de7303153
Dannil <danokhlopkov@gmail.com>
2018-05-09 03:30:52 +0300

    add script to update all dbmedias

---
+++ update_medias.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+
+from utils import *
+from instaget import *
+
+apis = get_apis()
+
+mipt_user_ids = extract_users_with_old_media_updates()
+
+for user_id in tqdm(mipt_user_ids):
+    if not download_user_medias(next(apis), user_id=user_id):
+        sleep(120)
+    sleep(60)
