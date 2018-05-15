7e8a5b8b9c69aff4de5494f0c2c69bafdf1a8e88
Dannil <danokhlopkov@gmail.com>
2018-05-15 17:13:32 +0300

    add json medias endpoint

--- instachart.py
+++ instachart.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 application = Flask(__name__)
 
 from dbextract import extract_scores
@@ -9,6 +9,11 @@ def homepage():
     return render_template("index.html", medias=medias[:10],
                            last_update=min([m["last_update"] for m in medias]),
                            total_mipt_users=len(set([m["username"] for m in medias])))
+
+@application.route("/medias")
+def media_data():
+    medias = extract_scores()
+    return jsonify(medias[:10])
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", debug=True, use_reloader=True)
